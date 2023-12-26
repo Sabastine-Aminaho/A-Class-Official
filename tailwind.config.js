424f721f54af5acaaf3049f17f9d73d30f2675af
Ndukaegho <sabastineaminaho3@gmail.com>
2023-12-26 13:05:12 +0100

    navbar commit

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: {
+        300: '#93c5fd',
+        400: '#60a5fa',
+        500: '#3b82f6',
+        600: '#2563eb',
+        700: '#1d4ed8',
+        800: '#1e40af',
+        900: '#1e3a8a',
+        950: '#172554',
+      },
+      secondary: {
+        100: '#f3f4f6',
+        200: '#e5e7eb',
+        300: '#d1d5db',
+        400: '#9ca3af',
+        500: '#6b7280',
+        600: '##4b5563',
+      }
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
+    fontFamily: {
+      body: ['Poppins']
+    },
+  },
+  plugins: [],
+}
+
