bde6a6024423c372838ecae5e9826cdbf41e703c
Ndukaegho <sabastineaminaho3@gmail.com>
2024-01-08 23:31:40 +0100

    home

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     extend: {},
     colors: {
       primary: {
+        50: '#eff6ff',
+        100: '#dbeafe',
+        200: '#bfdbfe',
         300: '#93c5fd',
         400: '#60a5fa',
         500: '#3b82f6',
@@ -34,7 +37,7 @@ module.exports = {
       'xl': '1280px',
     },
     fontFamily: {
-      body: ['Poppins']
+      Poppins: ['Poppins'],
     },
   },
   plugins: [],
